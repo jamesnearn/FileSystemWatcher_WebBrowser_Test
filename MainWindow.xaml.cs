7b52315ce7f943b57e804619f09380a0f151e683
James Nearn <github@nearn.org>
2023-03-01 22:07:26 -0500

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace FileSystemWatcher_WebBrowser_Test
+{
+    public partial class MainWindow : Window
+    {
+        FileSystemWatcher fsw = new FileSystemWatcher("c:\\temp");
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Debug.WriteLine("Constructor");
+
+            fsw.Created += Fsw_Created;
+            fsw.EnableRaisingEvents = true;
+        }
+
+        private void Fsw_Created(object sender, FileSystemEventArgs e)
+        {
+            Debug.WriteLine("Created");
+
+            // https://stackoverflow.com/questions/9732709/the-calling-thread-cannot-access-this-object-because-a-different-thread-owns-it
+            Dispatcher.Invoke(() =>
+            {
+                browser.Navigate("https://sirocco.accuweather.com/nx_mosaic_640x480_public/sir/inmsirmi_.gif");
+            });
+        }
+
+        private void google_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Click");
+
+            browser.Navigate("https://www.google.com");
+        }
+    }
+}
